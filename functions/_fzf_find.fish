cff6d4b55dbd05b7dcb16172e1973aeaf73206bc
Mirek Nguyen <nguyemir@fit.cvut.cz>
2022-10-12 12:52:58 +0200

    fix: convert exported variable to local

--- functions/_fzf_find.fish
+++ functions/_fzf_find.fish
@@ -1,6 +1,19 @@
 function _fzf_find
-  export FZF_DEFAULT_OPTS='--cycle --preview-window=right,40% --height 30% --border rounded --color='bg:#4B4B4B,bg+:#3F3F3F,info:#BDBB72,border:#6B6B6B,spinner:#98BC99,hl:#719872,fg:#D9D9D9,header:#719872,fg+:#D9D9D9,pointer:#E12672,marker:#E17899,prompt:#98BEDE,hl+:#98BC99'';
-  set -l fzf "$(fd --base-directory="$HOME" --strip-cwd-prefix -H -t f -t d | fzf -e --preview="bat --style=plain --theme gruvbox-dark --color=always "$HOME"/{} 2>/dev/null || exa --icons --oneline -g "$HOME"/{} 2>/dev/null")";
+  set -l color $(string join '' \
+    "bg:#4B4B4B,bg+:#3F3F3F,"\
+    "fg:#D9D9D9,fg+:#D9D9D9,"\
+    "hl:#719872,hl+:#98BC99,"\
+    "info:#BDBB72,border:#6B6B6B,"\
+    "spinner:#98BC99,header:#719872,"\
+    "pointer:#E12672,marker:#E17899,"\
+    "prompt:#98BEDE")
+  set -l preview \
+    "bat --style=plain --theme gruvbox-dark --color=always "$HOME"/{} 2>/dev/null \
+    || exa --icons --oneline -g "$HOME"/{} 2>/dev/null"
+  set -l fzf "$(\
+    fd --base-directory="$HOME" --strip-cwd-prefix -H -t f -t d \
+    | fzf -e --preview="$preview" --cycle --preview-window=right,40% --height 30% --border rounded --color="$color" \
+  )";
   if [ "$fzf" = "" ]
     commandline --function repaint;
     return 0;
--- functions/_fzf_find_notes.fish
+++ functions/_fzf_find_notes.fish
@@ -1,14 +1,24 @@
 function _fzf_find_notes
   set -l path "/Users/mireknguyen/Library/Mobile Documents/com~apple~CloudDocs/Notes";
-
-  export FZF_DEFAULT_OPTS='--cycle --preview-window=right,40% --height 30% --border rounded --color='bg:#4B4B4B,bg+:#3F3F3F,info:#BDBB72,border:#6B6B6B,spinner:#98BC99,hl:#719872,fg:#D9D9D9,header:#719872,fg+:#D9D9D9,pointer:#E12672,marker:#E17899,prompt:#98BEDE,hl+:#98BC99'';
   if ! [ -d "$path" ]
     commandline --function repaint;
     return 1;
   end
-
+  set -l color $(string join '' \
+    "bg:#4B4B4B,bg+:#3F3F3F,"\
+    "fg:#D9D9D9,fg+:#D9D9D9,"\
+    "hl:#719872,hl+:#98BC99,"\
+    "info:#BDBB72,border:#6B6B6B,"\
+    "spinner:#98BC99,header:#719872,"\
+    "pointer:#E12672,marker:#E17899,"\
+    "prompt:#98BEDE")
+  set -l preview \
+    "tail -n +(echo {} | cut -d':' -f2) < (echo {} | cut -d':' -f1)"
   set -l number $(echo $(echo "$path" | awk -F'/' '{print NF}') + 1 | bc);
-  set -l fzf $(rg -n -H . $path | fzf -e --delimiter / --with-nth $number.. --preview "tail -n +(echo {} | cut -d':' -f2) < (echo {} | cut -d':' -f1)");
+  set -l fzf \
+    $(rg -n -H . $path \
+    | fzf -e --delimiter / --with-nth $number.. --preview="$preview" \
+      --cycle --preview-window=right,40% --height 30% --border rounded --color="$color");
   if [ "$fzf" = "" ]
     commandline --function repaint;
     return 0;
