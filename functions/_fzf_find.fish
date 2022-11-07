7416d28cd75d80603ebdb02d86b29e55e97219ae
Mirek Nguyen <nguyemir@fit.cvut.cz>
2022-11-07 14:31:48 +0100

    feat: set preview as optional

--- functions/_fzf_find.fish
+++ functions/_fzf_find.fish
@@ -4,9 +4,10 @@ function _fzf_find
     "fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9,"\
     "info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6,"\
     "marker:#ff79c6,spinner:#ffb86c,header:#6272a4")
-  set -l preview \
-    "bat --style=plain --theme gruvbox-dark --color=always "$HOME"/{} 2>/dev/null \
-    || exa --icons --oneline -g "$HOME"/{} 2>/dev/null"
+  # set -l preview \
+  #   "bat --style=plain --theme gruvbox-dark --color=always "$HOME"/{} 2>/dev/null \
+  #   || exa --icons --oneline -g "$HOME"/{} 2>/dev/null"
+  set -l preview ""
   set -l fzf "$(\
     fd --base-directory="$HOME" --strip-cwd-prefix -H -t f -t d \
     | fzf -e --preview="$preview" --cycle --preview-window=right,40% --height 30% --border rounded --color="$color" \
