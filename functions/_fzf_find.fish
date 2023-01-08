3bcc1dc27ed46de54771f8a44869f48b2556be75
Mirek Nguyen <nguyemir@fit.cvut.cz>
2023-01-08 01:37:54 +0100

    feat: disable gitignore pattern in folders

--- functions/_fzf_find.fish
+++ functions/_fzf_find.fish
@@ -5,7 +5,7 @@ function _fzf_find
     "info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6,"\
     "marker:#ff79c6,spinner:#ffb86c,header:#6272a4")
   set -l fzf "$(\
-    fd --base-directory="$fish_fzf_find_scope" --ignore-file="$fish_fzf_find_ignore_file" --strip-cwd-prefix -H -t f -t d \
+    fd -I --base-directory="$fish_fzf_find_scope" --ignore-file="$fish_fzf_find_ignore_file" --strip-cwd-prefix -H -t f -t d \
     | fzf -e --preview="$fish_fzf_find_preview" --cycle --preview-window="$fish_fzf_find_preview" --height 30% --border rounded --color="$color" \
   )";
   if [ "$fzf" = "" ]
